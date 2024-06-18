a6c646cddec2b00a13d46a7c5eb725f08d8624b3
TMaltha <t.maltha@student.avans.nl>
2024-06-18 15:09:37 +0200

    made setup changes, should be ready for programming

---
+++ WuwaTrackerBackend/Models/WuwaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WuwaTrackerBackend.Models
+{
+    public class WuwaContext : DbContext
+    {
+        public WuwaContext(DbContextOptions<WuwaContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- WuwaTrackerBackend/Program.cs
+++ WuwaTrackerBackend/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using WuwaTrackerBackend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<WuwaContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("WuwaContext")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
