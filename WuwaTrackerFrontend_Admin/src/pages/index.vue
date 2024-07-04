5b903022fa890be5d2c1350037600c4b3b9b0e88
TMaltha <t.maltha@student.avans.nl>
2024-07-04 17:29:58 +0200

    installed acios

---
+++ WuwaTrackerFrontend_Admin/src/pages/index.vue
@@ -0,0 +1,125 @@
+<template>
+  <div style="padding: 50px">
+    <v-card title="Characters" variant="tonal">
+      <v-data-table :items-per-page="5" :items="items"></v-data-table>
+      <v-card-actions>
+        <v-btn>Add</v-btn>
+        <v-btn>Remove</v-btn>
+        <v-btn>Edit</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+const items = [
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  }]
+</script>
